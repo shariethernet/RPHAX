b04aeb5b6ab2b4be7163d43a4819b6c8a9c88c88
Shrihari <shariethernet@gmail.com>
2023-02-25 17:21:56 -0600

    RPHAX Connect v1

--- rphax_connect/rphaxFPGAConnect.py
+++ rphax_connect/rphaxFPGAConnect.py
@@ -6,83 +6,147 @@ import time
 import argparse
 import webbrowser
 import connectionparams as cp
+import requests
+import json
 
-#def find_matches(d, item):
-#    for k in d:
-#        if re.match(k, item):
-#            return d[k]
 
-config_yaml="""
-
+########### DO NOT MODIFY #################
+cf = CloudFlare.CloudFlare(token=cp.API_KEY)
+api_key=cp.API_KEY
+#api_email=cp.API_EMAIL
+zone_info = cf.zones.get(params={'name':cp.DOMAIN})
+#print("Zones info ",zone_info)
+zone_id = zone_info[0]['id']
+final_cname=""
+config_yaml="""\
 tunnel: {v_tunnel}
 credentials-file: {v_credentials_file}
 
 ingress:
     - hostname: {v_full_url}
-      service: {v_service_url}
-    - service: https_status:404
-
+      service: http://{v_service_url}:9090
+    - service: http_status:404
 """
 public_url=""
+############################################
+def extract_tunnel_credentials(string):
+    pattern = r"Tunnel credentials written to (\S+\/\S+\.json).* with id (\S+)"
+    match = re.search(pattern, string, re.DOTALL)
+    if match:
+        file_path = match.group(1)
+        unique_id = match.group(2)
+        return (file_path, unique_id)
+    else:
+        return None
 
-def gen_config_file(config_yml_string):
-    with open("config.yml",'w') as f:
+def isDuplicateCNAME(name,list1,list2):
+    if name in list1 or name in list2:
+        return True
+    else:
+        return False
+
+def gen_config_file(config_yml_string,config_fpath):
+    print("\n === Writing configuration file ===")
+    with open(config_fpath,'w') as f:
         f.write(config_yml_string)
     
 def cloudflared_installation_part1():
+    print("\n === Starting Cloudflared Download ===")
     try:
         os.system("cd /home/xilinx")
-        os.system("mkdir cloudflared")
+        os.system("mkdir -p cloudflared")
         os.system("cd cloudflared")
         os.system("wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm.deb")
+        print("\n === Finished Cloudflared Download ===")
     except:
-        print("Error 1")
+        raise Exception("Error downloading cloudflared")
     try:
-        os.system("dpkg --add-architecture arm")
-        os.system("dpkg -i cloudflared-linux-arm.deb")
+        print("\n === Starting Cloudflared Installation ===")
+        os.system("sudo dpkg --add-architecture arm")
+        os.system("sudo dpkg -i cloudflared-linux-arm.deb")
+        print("\n === Finished Cloudflared Installation ===")
     except:
-        print("Error insalling cloudflared")
+        raise Exception("Error insalling cloudflared")
 
-def tunnel_setup(pynq_hostname,outside_url):
+def tunnel_setup_part1():
+    '''
+    global api_key
+    global api_email
+    global zone_id
+    global final_cname
+    '''
+    print("\n === Cloudflare Authorization ===")
+    try:
+        os.system("mkdir -p $HOME/.cloudflared")
+    except:
+        raise Exception("Could not create .cloudflared directory")
+        '''
+    api_url = 'https://api.cloudflare.com/client/v4/accounts/{}/tokens'.format(api_key)
+    api_headers = {'X-Auth-Email': api_email, 'X-Auth-Key': api_key, 'Content-Type': 'application/json'}
+    api_data = {'name': 'Cloudflare Tunnel Token', 'policies': [{'id': 'Tunnel:Edit', 'resource': {'type': 'zone', 'id': zone_id}}]}
+    api_response = requests.post(api_url, headers=api_headers, data=json.dumps(api_data))
+    api_token = api_response.json()['result']['value']
 
+    # Authorize the Tunnel to the domain using the API token
+    api_url = 'https://api.cloudflare.com/client/v4/zones/{}/tunnels'.format(zone_id)
+    api_headers = {'Authorization': 'Bearer {}'.format(api_token), 'Content-Type': 'application/json'}
+    api_data = {'name': final_cname, 'credentials': {}}
+    api_response = requests.post(api_url, headers=api_headers, data=json.dumps(api_data))
+
+    if api_response.status_code == 200:
+        print('Cloudflare Tunnel authorized successfully.')
+    else:
+        raise Exception('Cloudflare Tunnel authorization failed.')
+'''
     result = subprocess.run(['cloudflared', 'tunnel', 'login'], capture_output=True, text=True)
     if(result.returncode!=0):
+        print(result.stdout)
         if 'https://' in result.stdout:
         # If a URL is present, open it in the default web browser
             url = result.stdout.strip()
-            print("Open this url:")
+            print("\nOpen this url:")
             #webbrowser.open(url)
 
-    # Wait for user to authenticate
-    start_time = time.time()
-    while True:
-        time.sleep(5)  # Wait for 5 seconds
-        check_result = subprocess.run(['cloudflared', 'tunnel', 'whoami'], capture_output=True, text=True)
-        if 'Logged in as' in check_result.stdout:
-            break
-        elif time.time() - start_time >= 600:  # Timeout after 10 minutes
-            print("Authentication timed out after 10 minutes.")
-            exit(1)
-
     # Run the "cloudflared tunnel create test" command after successful authentication
-    tunnel_create = subprocess.run(['cloudflared', 'tunnel', 'create', 'test1'],capture_output=True, text =True)
-    print(tunnel_create.stdout)
-    config_yaml.format(v_tunnel="",
-                   v_credentials_file="",
-                   v_full_url="",
-                   v_service_url=pynq_hostname)
+def tunnel_setup_part2(cname,pynq_hostname,config_yml_string):
+    global final_cname
+    global tunnel_id
+    tunnel_create = subprocess.run(['cloudflared', 'tunnel', 'create', cname],capture_output=True, text =True)
+    tunnel_create_out = tunnel_create.stdout
+    tunnel_create_err = tunnel_create.stderr
+    if("tunnel with name already exists" in tunnel_create_out):
+        raise Exception("Tunnel Name exists")
+    else:
+        print(tunnel_create_out,tunnel_create_err)
+        cred_file,tunnel_id = extract_tunnel_credentials(tunnel_create_out)
 
-def cloudflared_installation_part2():
+        a=config_yml_string.format(v_tunnel=tunnel_id,
+                       v_credentials_file=cred_file,
+                       v_full_url=final_cname+'.'+cp.DOMAIN,
+                       v_service_url=pynq_hostname)
+
+        print(a)
+        p=r'(.*)/[^/]*$'
+        match=re.search(p,cred_file)
+        dir_path=match.group(1)
+        gen_config_file(a,dir_path+"/config.yml")
+
+def cloudflared_service_install():
     try:
-        os.system("cloudflared service install")
-        os.system("systemctl start cloudflared")
-        os.system("systemctl status cloudflared")
+        print("\n === Installing Cloudflared as a system service ===")
+        os.system("sudo cp /home/xilinx/.cloudflared/config.yml /etc/cloudflared/config.yml")
+        os.system("sudo cloudflared service install")
+        os.system("sudo systemctl start cloudflared")
+        #os.system("sudo systemctl status cloudflared")
+        print("\n === Cloudflared as a system service is active ===")
     except:
-        print("\n Error in Part2")
+        print("\n !!! Error installing cloudflared as a service !!!")
 
-def cloudflare_dns_config(cname,domain):
+def cloudflare_dns_read(cname,domain):
+    global final_cname
+    global public_url
     zone_name = domain
-    cf = CloudFlare.CloudFlare(token=cp.API_KEY)
+
     zone_info = cf.zones.get(params={'name':zone_name})
     #print("Zones info ",zone_info)
     zone_id = zone_info[0]['id']
@@ -112,7 +176,11 @@ def cloudflare_dns_config(cname,domain):
     print("Pynq FPGA List:",pynqfpga_list)
     print("----------------------------------------------------")
     pynqfpga_list.sort(reverse=True)
-    recentfpgamember= pynqfpga_list[0]
+    try:
+        recentfpgamember= pynqfpga_list[0]
+    except:
+        pynqfpga_list.append("u_0_pynq."+zone_name)
+        recentfpgamember=pynqfpga_list[0]
     print("\n Recent FPGA Member",recentfpgamember)
     recentfpgamember_number = recentfpgamember.split("_")[1]
     print("\n Recent FPGA member number :",recentfpgamember_number)
@@ -121,17 +189,25 @@ def cloudflare_dns_config(cname,domain):
     nextfpgamember="u_"+str(nextfpgamember_number)+"_pynq."+zone_name
     print("\n Current FPGA URL :",nextfpgamember)
     print("----------------------------------------------------")
-    cnamerecordname ="u_"+str(nextfpgamember_number)+"_pynq" 
-    in_str=cname+'.'+zone_name
-    public_url=in_str
+    gen_cnamerecordname ="u_"+str(nextfpgamember_number)+"_pynq" 
+    in_str=cname+"."+domain
     if(isDuplicateCNAME(in_str,argotunnel_list,pynqfpga_list) or cname == " "):
-        #write_dns_record = {'name':cnamerecordname,'type':'CNAME','content':cnamerecordname+'.cfargotunnel.com'}
-        #cf.zones.dns_records.post(zone_id, data=write_dns_record)
+        final_cname = gen_cnamerecordname
+        public_url=final_cname+'.'+zone_name
         print("CNAME conflict or absent")
     else:
-        #write_dns_record = {'name':cnamerecordname,'type':'CNAME','content':cname+'.cfargotunnel.com'}
-        #cf.zones.dns_records.post(zone_id, data=write_dns_record)
+        final_cname = cname
+        public_url=final_cname+'.'+zone_name
         print("Custom CNAME Record added successfully")
+    print(f"public_url: {public_url}".format(public_url=public_url))
+
+def cloudflare_dns_write(domain,tunnel_id_a):
+    zone_name = domain
+    zone_info = cf.zones.get(params={'name':zone_name})
+    #print("Zones info ",zone_info)
+    zone_id = zone_info[0]['id']
+    write_dns_record = {'name':final_cname,'type':'CNAME','content':tunnel_id+'.cfargotunnel.com'}
+    cf.zones.dns_records.post(zone_id, data=write_dns_record)
 
 
     '''
@@ -149,21 +225,17 @@ def cloudflare_dns_config(cname,domain):
 def main():
     parser = argparse.ArgumentParser(description = "RPHAX Remote - Configurator")
     parser.add_argument('-cn','--cname',type=str,help = "User Defined CNAME for the record", default=" ")
-    parser.add_argument('-cp','--configpath',type=str,help = "Cloudflared config file path", default=" ")
+    parser.add_argument('-cp','--configpath',type=str,help = "Cloudflared config file path", default="$HOME/.cloudflared/config.yml")
     parser.add_argument('pynq_hostname',type=str,help="Provide the localhost name of pynq along with the port number Eg: pynq:9090")
     args = parser.parse_args()
 
     cloudflared_installation_part1()
-    cloudflare_dns_config(args.cname,cp.DOMAIN)
-    tunnel_setup(args.pynq_hostname,public_url)
-    cloudflared_installation_part2()
-
-def isDuplicateCNAME(name,list1,list2):
-    if name in list1 or name in list2:
-        return True
-    else:
-        return False
-
+    cloudflare_dns_read(args.cname,cp.DOMAIN)
+    tunnel_setup_part1()
+    print(final_cname)
+    tunnel_setup_part2(final_cname,args.pynq_hostname,config_yaml)
+    cloudflare_dns_write(cp.DOMAIN,tunnel_id)
+    cloudflared_service_install()
 
 if __name__ == "__main__":
     main()
--- rphax_connect/test.py
+++
@@ -1,7 +0,0 @@
-import re
-a = "94b73058-f390-4312-ae0d-6607106f51f8.cfargotunnel.com"
-a_split = a.split(".")
-if a_split[1] == "cfargotunnel":
-    print("match")
-else :
-    print("No Match")
